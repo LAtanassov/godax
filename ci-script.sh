587ccad95e736d660726a38eccfa6c22d5432f70
latanassov <latschesar.atanassov@gmx.at>
2018-09-27 20:47:34 +0200

    rm unneccessary cli cmd

--- ci-script.sh
+++ ci-script.sh
@@ -2,7 +2,6 @@
 # used for development
 
 GO_FILES=$(find . -iname '*.go' -type f | grep -v /vendor/) # All the .go files, excluding vendor/
-# go get golang.org/x/vgo
 
 go test -v -race ./...                  # Run all the tests with the race detector enabled
 go vet ./...                            # go vet is the official Go static analyzer
@@ -13,7 +12,6 @@ go get github.com/golang/lint/golint     # Linter
 go get honnef.co/go/tools/cmd/megacheck  # Badass static analyzer/linter
 go get github.com/fzipp/gocyclo
 
-go mod -vendor
 megacheck ./...                          # "go vet on steroids" + linter
 gocyclo -over 19 $GO_FILES               # forbid code with huge functions
 golint -set_exit_status $(go list ./...) # one last linter
